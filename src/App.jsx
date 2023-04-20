e92f4536bcc421041a18be366e9ab1349e67ddbe
Mike Livis <mike@pau.be>
2023-04-20 11:10:18 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./styles/global.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-red-500">MarketHub</h1>
+    </div>
+  );
+}
+
+export default App;
