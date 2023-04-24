2e223dc7b08a48bf5428d40bae632ff540d7e05d
Andres Vergauwen <andres.vergauwen@hotmail.com>
2023-04-24 10:11:17 +0200

    feat: added vite-pwa and some icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  server: {
+    port: 3000
+  },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "MarketHub",
+        short_name: "MarketHub",
+        description: "MarketHub Webshop, maintained and hosted by Andres Vergauwen & Mike Livis",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ],
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone"
+      }
+    })
+  ]
 })
