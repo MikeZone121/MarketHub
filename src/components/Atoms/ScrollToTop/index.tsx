e3f7962ac56966c6bc177e49cc92afadf4d43bfe
Andres Vergauwen <andres.vergauwen@hotmail.com>
2023-05-08 21:53:30 +0200

    feat: smoothly scroll to top

--- src/components/Atoms/ScrollToTop/index.tsx
+++ src/components/Atoms/ScrollToTop/index.tsx
@@ -5,7 +5,11 @@ const ScrollToTop = () => {
   const { pathname } = useLocation()
 
   useEffect(() => {
-    window.scrollTo(0, 0)
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth"
+    })
   }, [pathname])
   return <></>
 }
