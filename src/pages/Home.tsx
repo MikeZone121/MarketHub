5d7ead29536346885d97bbbd1398615a8a81c7dc
Andres Vergauwen <andres.vergauwen@hotmail.com>
2023-05-08 12:54:02 +0200

    feat: renamed items to products

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,7 @@ function Home() {
   return (
     <div className="tw-my-10">
       <article className="tw-flex tw-flex-wrap tw-items-center tw-justify-center">
-        <Hero items={data?.products} isLoading={isLoading} />
+        <Hero products={data?.products} isLoading={isLoading} />
       </article>
       <article className="tw-m-auto tw-flex tw-w-11/12 tw-max-w-screen-2xl tw-flex-col tw-justify-center">
         <Title size={TitleSizeEnum.H4} text="Popular items" className="!tw-font-bold !tw-text-gray-700" />
